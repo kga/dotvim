1812c61f8ac833f8a4b4abb88dd773a57368f578
Wataru TOYA <watrty@gmail.com>
2026-01-31 16:36:10 +0900

    Revert "Simplify lualine config to only specify diffs from default"
    This reverts commit ed2844e8ad63c185204cd985072db525b7bc5fe0.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,12 +7,20 @@ return {
         theme = "nord",
       },
       sections = {
-        -- デフォルトの diagnostics を除外（LSP 診断はコード上のサインで十分）
+        lualine_a = { "mode" },
         lualine_b = { "branch", "diff" },
+        lualine_c = { { "filename", path = 0 } },
+        lualine_x = { "fileformat", "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
       inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
         lualine_c = { { "filename", path = 1 } },
         lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     },
   },
